b2620be353719dbc90848575aa696c1b8ae958f2
陈宇峰 <yufengcode@gmail.com>
2016-08-08 13:50:36 +0800

    Initial Commit

---
+++ DeleteNodeInO(1)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DeleteNodeInO(1)
+//
+//  Created by chenyufeng on 8/8/16.
+//  Copyright © 2016 chenyufeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
